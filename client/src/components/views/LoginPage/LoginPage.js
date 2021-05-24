fcc9a78f38e84e5be1d02f58f49dcef5e147afa5
118dg <118dg@naver.com>
2021-05-24 18:27:52 +0900

    part30 6:33까지
    user_action.js 수정해야 함!

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 
-function LoginPage() {
-  return (
-    <div>
-      LoginPage
-    </div>
-  );
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    //State
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    //State Handler
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault(); //아무것도 하지 않았을 때 refresh 되는 것 방지
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    props.history.push('/')
+                } else {
+                    alert('Error˝')
+                }
+            })
+    }
+
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    )
 }
 
-export default LoginPage;
+export default withRouter(LoginPage)
